41e8a46b316093c5487a58d3acadda2b40a45052
KimJeongHoon3 <kimjh112339@naver.com>
2023-02-03 18:36:56 +0900

    [item1] FlyweightPattrn 관련 스터디 내용 추가

--- src/main/java/com/study/effectivejava/item1/FlyweightPattern.java
+++ src/main/java/com/study/effectivejava/item1/FlyweightPattern.java
@@ -1,8 +1,52 @@
 package com.study.effectivejava.item1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlyweightPattern {
 
-    public static void main(String[] args) {
+    public static class OperatorFactory {
+        private final static Map<String, Operator> cached = new HashMap<>();
 
+        private OperatorFactory() {}
+
+        public static Operator getOperator(String operateCode) {
+            Operator operator = cached.get(operateCode);
+
+            if (operator == null) {
+                if (operateCode.equals("+")) {
+                    operator = new PlusOperator();
+                } else if (operateCode.equals("-")) {
+                    operator = new MinusOperator();
+                }
+                cached.put(operateCode, operator);
+            }
+
+            if (operator == null) {
+                throw new IllegalArgumentException();
+            }
+
+            return operator;
+        }
     }
+
+    interface Operator {
+        int operate(int leftOperand, int rightOperand);
+    }
+
+    public static class PlusOperator implements Operator {
+        @Override
+        public int operate(int leftOperand, int rightOperand) {
+            return leftOperand + rightOperand;
+        }
+    }
+
+    public static class MinusOperator implements Operator {
+
+        @Override
+        public int operate(int leftOperand, int rightOperand) {
+            return leftOperand - rightOperand;
+        }
+    }
+
 }
--- src/test/java/com/study/effectivejava/item1/FlyweightPatternTest.java
+++ src/test/java/com/study/effectivejava/item1/FlyweightPatternTest.java
@@ -7,6 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class FlyweightPatternTest {
     @Test
     void test() {
+        /*
+            flyweightPattern
+            - 캐싱과 동일개념
+            - 팩토리에서 인스턴스를 반환해줄때 팩토리에 저장되어있는 객체면 새로 생성하지않고, 기존에 저장되어있는것 반환. 그렇지않으면 새로생성하여 내부에 저장한뒤 반환
+        */
+
+        FlyweightPattern.Operator first = FlyweightPattern.OperatorFactory.getOperator("+");
+        FlyweightPattern.Operator second = FlyweightPattern.OperatorFactory.getOperator("+");
+
+        assertEquals(3, first.operate(1, 2));
+        assertTrue(first == second); // 캐싱을해서 가져왔기때문에 주소도 같아야한다
 
     }
 }
